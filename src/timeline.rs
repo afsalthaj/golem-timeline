b280eaadc448a48d26a1a48add2fa5e8163a0eaa
Afsal Thaj <afsal.taj06@gmail.com>
2024-02-04 21:04:24 +1100

    Add more info in the DAG

--- src/timeline.rs
+++ src/timeline.rs
@@ -41,84 +41,23 @@ impl<T> TimeLine<T> {
 }
 
 
-// Each o the below functions invokes a worker
-// Each worker is responsible for forgetting past beyond an extent
-// This limitation exists in any real world system
-// This is more of tracking a StateDynamic event, as a cumulative OR
-// Input
-// t1: false
-// t2: true
-// t3: false
-
-// Output
-// t1-t2: false
-// t2-t3: true
 fn tl_has_existed<T>(event_stream: event_predicate: EventPredicate) -> TimeLine<bool> {
 
 }
 
-// This is more of tracking a StateDynamic event, as a cumulative OR
-// Input
-// Duration: D = 4
-// t1: false
-// t3: true
-// t9: true
-
-// Output
-// t1-t3: false
-// t3-t7: true
-// t7-t9: false
-// t9-t13: true
 fn tl_has_existed_within<T>(event_predicate: EventPredicate) -> TimeLineStream<bool> {
     unimplemented!("tl_has_existed_within not implemented")
 }
 
 
-// This is more or less making number of events to a very simple
-// timeline. Obviously this is corresponding to the events that are state dynamic in nature
-// t1 - t10 : CDN2
-// t10 - t11 : CDN1
-// t11- t12: CDN1
-
-//Output
-// t1-t10: CDN2
-// t10-t12: CDN1
 fn tl_latest_event_to_state<T>(event_predicate: EventPredicate) -> TimeLineStream<bool> {
     unimplemented!("tl_latest_event_to_state not implemented")
 }
 
-// A Numerical Timeline of
-// the cumulative duration
-// where the state was True
-// t1 - t3: false
-// t3 - t8: true
-// t8 - t14: false
-// t14 - t20: true
-
-
-// Output
-// t1 - t3: 0
-// t3 - t8 : 5
-// t8 - t4 : 5
-// t14 - t20: 11
 fn tl_duration_where<T>(event_predicate: EventPredicate) -> TimeLineStream<u64> {
     unimplemented!("tl_duration_where not implemented")
 }
 
-// A Numerical Timeline of
-// the duration since the last
-// state change
-
-// t1-t3: buffer
-// t3-t8: play
-// t8-t14: buffer
-// t14-t20: pause
-
-// Output
-// t1-t3: 3
-// t3- t8: 5
-// t8-t14: 6
-// t14- t20: 6
 fn tl_duration_in_cur_state<T>() -> TimeLineStream<u64> {
     unimplemented!("tl_duration_in_cur_state not implemented")
 }
--- src/timeline_op.rs
+++ src/timeline_op.rs
@@ -5,18 +5,77 @@ use crate::timeline::TimeLine;
 enum TimeLineOp<T> {
     // Essentially based on paper, there is only numerical timeline and state dynamic timeline
     // A state dynamic is pretty much state that is dynamic. Consider this as a constant value
+    // At this stage of development, I am not thinking of state dynamic in TimeLineOp, as I am not
+    // sure of the exact implication of skipping this concept, and I wanted to see how it goes
     // during the timeline, while numerical keeps moving
     // A numerical timeline essentially cannot be pattern matched, as it is a continuous value
+    // Refer paper to understand what these operations are
     EqualTo(TimeLineOp<T>, T),
     GreaterThan(TimeLineOp<T>, T),
     LessThan(TimeLineOp<T>, T),
     And(TimeLineOp<T>, TimeLineOp<T>),
     Or(TimeLineOp<T>, TimeLineOp<T>),
     Not(TimeLineOp<T>),
+
+     // Each o the below functions invokes a worker
+     // Each worker is responsible for forgetting past beyond an extent
+     // This limitation exists in any real world system
+     // This is more of tracking a StateDynamic event, as a cumulative OR
+     // Input
+     // t1: false
+     // t2: true
+     // t3: false
+     // Output
+     // t1-t2: false
+     // t2-t3: true
     TlHasExisted(TimeLineOp<T>, EventPredicate),
+    // This is more of tracking a StateDynamic event, as a cumulative OR
+    // Input
+    // Duration: D = 4
+    // t1: false
+    // t3: true
+    // t9: true
+    // Output
+    // t1-t3: false
+    // t3-t7: true
+    // t7-t9: false
+    // t9-t13: true
     TlHasExistedWithin(TimeLineOp<T>, EventPredicate),
+    // This is more or less making number of events to a very simple
+    // timeline. Obviously this is corresponding to the events that are state dynamic in nature
+    // t1 - t10 : CDN2
+    // t10 - t11 : CDN1
+    // t11- t12: CDN1
+    // Output
+    // t1-t10: CDN2
+    // t10-t12: CDN1
     TlLatestEventToState(TimeLineOp<T>, EventPredicate),
+    // A Numerical Timeline of
+    // the cumulative duration
+    // where the state was True
+    // t1 - t3: false
+    // t3 - t8: true
+    // t8 - t14: false
+    // t14 - t20: true
+    // Output
+    // t1 - t3: 0
+    // t3 - t8 : 5
+    // t8 - t4 : 5
+    // t14 - t20: 11
     TlDurationWhere(TimeLineOp<T>, EventPredicate),
+
+    // A Numerical Timeline of
+    // the duration since the last
+    // state change
+    // t1-t3: buffer
+    // t3-t8: play
+    // t8-t14: buffer
+    // t14-t20: pause
+    // Output
+    // t1-t3: 3
+    // t3- t8: 5
+    // t8-t14: 6
+    // t14- t20: 6
     TlDurationInCurState(TimeLineOp<T>),
 }
 
