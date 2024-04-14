91fe48d7e74dad378b805e37a3b87d66d761cc40
Afsal Thaj <afsal.taj06@gmail.com>
2024-04-14 20:05:48 +1000

    Implement raw events

--- raw-events/src/lib.rs
+++ raw-events/src/lib.rs
@@ -1,16 +1,40 @@
-use crate::bindings::exports::timeline::rawevents::api::{Guest, Order};
+use crate::bindings::exports::timeline::rawevents::api::{Event, Guest};
 
 mod bindings;
 
 struct Component;
 
+struct State {
+    orders: Vec<Event>,
+}
+
+static mut STATE: State = State {
+    orders: Vec::new()
+};
+
+fn with_state<T>(f: impl FnOnce(&mut State) -> T) -> T {
+    let result = unsafe { f(&mut STATE) };
+
+    return result;
+}
 
 impl Guest for Component {
-    fn add_order(order: Order) {
-        todo!()
+    fn add_event(order: Event) {
+        with_state(|state| {
+            state.orders.push(order);
+        });
     }
 
-    fn get_events() -> Vec<Order> {
-        todo!()
+    fn get_event(time: u64) -> Event {
+        with_state(|state| {
+            state.orders.iter().find(|event| event.time == time).unwrap().clone()
+        })
+    }
+
+    fn get_events() -> Vec<Event> {
+        with_state(|state| {
+            state.orders.clone()
+        })
     }
 }
+
