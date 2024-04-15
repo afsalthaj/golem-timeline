cc5c4b74b0e84e79a933bb98a97a19cf122f3cce
Afsal Thaj <afsal.taj06@gmail.com>
2024-04-15 11:41:39 +1000

    Start adding worker dependency

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -16,15 +16,30 @@ pub mod zip_result;
 
 use crate::bindings::exports::timeline::core::api::Guest;
 use crate::bindings::exports::timeline::core::api::*;
+use crate::timeline_op::TimeLineOp as CoreTimeLineOp;
 
 struct Component;
 
 impl Guest for Component {
     fn initialize_timeline(timeline: TimelineOp) {
-        dbg!(timeline);
-       //let timeline: crate::timeline_op::TimeLineOp = timeline.into();
+       let timeline: CoreTimeLineOp = timeline.into();
 
-       // dbg!(timeline.to_string());
+       match timeline {
+           CoreTimeLineOp::Leaf() => {}
+           CoreTimeLineOp::EqualTo(_, _) => {}
+           CoreTimeLineOp::GreaterThan(_, _) => {}
+           CoreTimeLineOp::GreaterThanOrEqual(_, _) => {}
+           CoreTimeLineOp::LessThan(_, _) => {}
+           CoreTimeLineOp::LessThanOrEqual(_, _) => {}
+           CoreTimeLineOp::And(_, _) => {}
+           CoreTimeLineOp::Or(_, _) => {}
+           CoreTimeLineOp::Not(_) => {}
+           CoreTimeLineOp::TlHasExisted(_, _) => {}
+           CoreTimeLineOp::TlHasExistedWithin(_, _, _) => {}
+           CoreTimeLineOp::TlLatestEventToState(_, _) => {}
+           CoreTimeLineOp::TlDurationWhere(_, _) => {}
+           CoreTimeLineOp::TlDurationInCurState(_, _) => {}
+       }
 
         dbg!("Excuted timeline remotely");
     }
