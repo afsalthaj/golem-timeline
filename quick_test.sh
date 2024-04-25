751f411734c3dadf1d7bb41b1931e5b466898c30
Afsl Thaj <afsal.taj06@gmail.com>
2024-04-25 17:07:31 +1000

    Start fixing quick test

--- quick_test.sh
+++ quick_test.sh
@@ -6,9 +6,9 @@ current_epoch=$(date +%s)
 
 driver_with_core_template_name="driver_with_core${current_epoch}"
 
-core_composed_template_id=$(golem-cli -F json template add --template-name core_composed_all${current_epoch} target/wasm32-wasi/debug/core_composed_leaf.wasm | jq .templateId)
-event_processor_template_id=$(golem-cli -F json template add --template-name raw_event${current_epoch} target/wasm32-wasi/debug/event_processor.wasm | jq .templateId)
-driver_template_id=$(golem-cli -F json template add --template-name "$driver_with_core_template_name" target/wasm32-wasi/debug/driver_composed.wasm| jq .templateId)
+core_composed_template_id=$(golem-cli -F json component add --component-name core_composed_all${current_epoch} target/wasm32-wasi/debug/core_composed_leaf.wasm | jq .componentId)
+event_processor_template_id=$(golem-cli -F json component add --component-name raw_event${current_epoch} target/wasm32-wasi/debug/event_processor.wasm | jq .componentId)
+driver_template_id=$(golem-cli -F json component add --component-name "$driver_with_core_template_name" target/wasm32-wasi/debug/driver_composed.wasm| jq .componentId)
 
 echo "Template IDs:"
 echo "Core Composed: $core_composed_template_id"
@@ -16,7 +16,7 @@ echo "Raw Events: $event_processor_template_id"
 echo "Driver: $driver_template_id"
 
 # Construct the command with properly formatted parameters
-command="golem-cli worker invoke-and-await --template-id \"$driver_template_id\" --worker-name first-try --function timeline:driver/api/run --parameters '[$core_composed_template_id, $event_processor_template_id, \"dummy\"]'"
+command="golem-cli worker invoke-and-await --component-id \"$driver_template_id\" --worker-name first-try --function timeline:driver/api/run --parameters '[$core_composed_template_id, $event_processor_template_id, \"dummy\"]'"
 
 # Output the constructed command
 echo "A dry run on deployed timeline..."
@@ -36,7 +36,7 @@ api_definition='{
       "path": "/{user-id}/instantiate-timeline",
       "binding": {
         "type": "wit-worker",
-        "template": REPLACE_DRIVER_TEMPLATE_ID,
+        "component": REPLACE_DRIVER_TEMPLATE_ID,
         "workerId": "first-try",
         "functionName": "timeline:driver/api/run",
         "functionParams": [REPLACE_CORE_COMPOSED, REPLACE_event_processor, "dummy"],
@@ -69,7 +69,7 @@ deployment='{
 
 deployment="${deployment/REPLACE_VERSION/$current_epoch}"
 
-curl -X PUT http://localhost:9881/v1/api/deployments -H "Content-Type: application/json"  -d "$deployment"
+curl -X POST http://localhost:9881/v1/api/deployments/deploy -H "Content-Type: application/json"  -d "$deployment"
 
 echo ""
 echo "Deployment succeeded!"
