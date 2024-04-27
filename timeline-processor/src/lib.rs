fe30d5310e160d1ec80693e7d7b348b433c506f1
Afsl Thaj <afsal.taj06@gmail.com>
2024-04-27 15:42:41 +1000

    Compose timeline processor with event processor

--- timeline-processor/src/lib.rs
+++ timeline-processor/src/lib.rs
@@ -1,4 +1,6 @@
 use crate::bindings::exports::timeline::timeline_processor::api::{EventValue, Guest, WorkerId};
+use crate::bindings::timeline::event_processor_stub::stub_event_processor;
+use crate::bindings::golem::rpc::types::Uri;
 
 mod bindings;
 
@@ -6,6 +8,19 @@ struct Component;
 
 impl Guest for Component {
     fn initialize_equal(child_url: WorkerId, current_worker_id: WorkerId, event_value: EventValue) -> Result<String, String> {
-        todo!()
+
+
+
+        let uri = Uri {
+            value: format!("worker://some_template/{}", "some_worker"),
+        };
+
+        let core = stub_event_processor::Api::new(&uri);
+
+        core.tl_has_existed(
+           1
+        )?;
+
+        Ok("".to_string())
     }
 }
