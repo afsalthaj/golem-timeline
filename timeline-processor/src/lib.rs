07434e28d39ef82d10f51380750508ba855faa7b
Afsal Thaj <afsal.taj06@gmail.com>
2024-04-28 22:38:49 +1000

    Define structs for storing the worker id

--- timeline-processor/src/lib.rs
+++ timeline-processor/src/lib.rs
@@ -1,3 +1,6 @@
+use timeline::event_predicate::EventColumnName;
+use timeline::golem_event::GolemEventValue;
+use timeline::state_dynamic_timeline::StateDynamicsTimeLine;
 use crate::bindings::exports::timeline::timeline_processor::api::{EventValue, Guest, TypedTimelineResultWorker};
 use crate::bindings::timeline::event_processor_stub::stub_event_processor;
 //use crate::bindings::timeline::timeline_processor_stub::stub_timeline_processor;
@@ -6,6 +9,41 @@ use crate::bindings::golem::rpc::types::Uri;
 mod bindings;
 
 struct Component;
+
+struct TLEqual {
+    child_worker: TypedTimelineResultWorker,
+}
+
+struct TLGreaterThan {
+    child_worker: TypedTimelineResultWorker,
+}
+
+struct TLGreaterThanOrEqualTo {
+    child_worker: TypedTimelineResultWorker,
+}
+
+struct TLLessThan {
+    child_worker: TypedTimelineResultWorker,
+}
+
+struct TLLessThanOrEqualTo {
+    child_worker: TypedTimelineResultWorker,
+}
+
+struct TLAnd {
+    child_worker1: TypedTimelineResultWorker,
+    child_worker2: TypedTimelineResultWorker,
+}
+
+struct TLOr {
+    child_worker1: TypedTimelineResultWorker,
+    child_worker2: TypedTimelineResultWorker,
+}
+
+
+thread_local! {
+
+}
 
 impl Guest for Component {
     fn initialize_equal(child_url: TypedTimelineResultWorker, event_value: EventValue) -> Result<String, String> {
