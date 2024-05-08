267d3478ff59897e8bf8de2358472545a0f22b11
kmatas <33095685+kmatasfp@users.noreply.github.com>
2024-05-08 14:01:27 -0700

    rename template_id to component_id to match the naming on Golem side

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -1,12 +1,14 @@
-use std::fmt::format;
 use crate::bindings::exports::timeline::driver::api::Guest;
 use crate::bindings::golem::rpc::types::Uri;
+use crate::bindings::timeline::core::api::TimelineNode::{TimelineNegation, TlLatestEventToState};
 use crate::bindings::timeline::core::api::{Server, TypedTimelineResultWorker};
-use crate::bindings::timeline::core::api::TimelineNode::{TlLatestEventToState, TimelineNegation};
-use crate::bindings::timeline::core::api::{ServerWithEventColumnName, TimelineOp, TimelineNegated};
+use crate::bindings::timeline::core::api::{
+    ServerWithEventColumnName, TimelineNegated, TimelineOp,
+};
 use crate::bindings::timeline::core_stub::stub_core;
 use crate::bindings::timeline::timeline_processor::api::{DerivedTimelineNode, LeafTimelineNode};
 use conversions::Conversion;
+use std::fmt::format;
 
 #[allow(dead_code)]
 #[rustfmt::skip]
@@ -26,39 +28,56 @@ impl Guest for Component {
 
         let core = stub_core::Api::new(&uri);
         let timeline_op = TimelineOp {
-            nodes: vec![ TimelineNegation(TimelineNegated{
-                server: Server {
-                    template_id: timeline_processor_template_id.to_string(),
-                    worker_id_prefix: "cirr".to_string(),
-                },
-                timeline: 1
-            }),  TlLatestEventToState(ServerWithEventColumnName {
-                server: Server {
-                    template_id: event_processor_template_id.to_string(),
-                    worker_id_prefix: "cirr".to_string(),
-                },
-                event_column_name: "playerStateChange".to_string(),
-            })],
+            nodes: vec![
+                TimelineNegation(TimelineNegated {
+                    server: Server {
+                        template_id: timeline_processor_template_id.to_string(),
+                        worker_id_prefix: "cirr".to_string(),
+                    },
+                    timeline: 1,
+                }),
+                TlLatestEventToState(ServerWithEventColumnName {
+                    server: Server {
+                        template_id: event_processor_template_id.to_string(),
+                        worker_id_prefix: "cirr".to_string(),
+                    },
+                    event_column_name: "playerStateChange".to_string(),
+                }),
+            ],
         };
 
         match core.initialize_timeline(&timeline_op) {
             Ok(result) => {
                 dbg!("Driver Log: Timeline initialized");
 
-                let serializable_result_worker =
-                    serde_json::to_value(timeline::timeline_node_worker::TypedTimeLineResultWorker::from_wit(result.result_worker)).map_err(|err| err.to_string())?;
+                let serializable_result_worker = serde_json::to_value(
+                    timeline::timeline_node_worker::TypedTimeLineResultWorker::from_wit(
+                        result.result_worker,
+                    ),
+                )
+                .map_err(|err| err.to_string())?;
 
                 let mut event_processors = vec![];
 
                 for worker in result.event_processor_workers.iter() {
-                    let event_processor = timeline::timeline_node_worker::TypedTimeLineResultWorker::from_wit(worker.clone());
-                    event_processors.push(serde_json::to_value(event_processor).map_err(|err| err.to_string())?);
+                    let event_processor =
+                        timeline::timeline_node_worker::TypedTimeLineResultWorker::from_wit(
+                            worker.clone(),
+                        );
+                    event_processors.push(
+                        serde_json::to_value(event_processor).map_err(|err| err.to_string())?,
+                    );
                 }
 
-                let driver_result: String = serde_json::Value::Object(serde_json::Map::from_iter(vec![
-                    ("result_worker".to_string(), serializable_result_worker),
-                    ("event_processors".to_string(), serde_json::Value::Array(event_processors)),
-                ])).to_string();
+                let driver_result: String =
+                    serde_json::Value::Object(serde_json::Map::from_iter(vec![
+                        ("result_worker".to_string(), serializable_result_worker),
+                        (
+                            "event_processors".to_string(),
+                            serde_json::Value::Array(event_processors),
+                        ),
+                    ]))
+                    .to_string();
 
                 Ok(driver_result)
             }
@@ -70,93 +89,79 @@ impl Guest for Component {
     }
 }
 
-fn print_typed_time_line_result_worker(typed_timeline_result_worker: TypedTimelineResultWorker) -> String {
+fn print_typed_time_line_result_worker(
+    typed_timeline_result_worker: TypedTimelineResultWorker,
+) -> String {
     match typed_timeline_result_worker {
-        TypedTimelineResultWorker::LeafTimeline(leaf_time_line_node) => {
-            match leaf_time_line_node {
-                LeafTimelineNode::TlHasExisted(timeline_result_worker) => {
-                    format!(
-                        "LeafTimeLine: TlHasExisted: Server: template_id: {}, worker_id: {}",
-                        timeline_result_worker.template_id,
-                        timeline_result_worker.worker_id,
-                    )
-                },
-                LeafTimelineNode::TlHasExistedWithin(timeline_result_worker) => {
-                    format!(
-                        "LeafTimeLine: TlHasExistedWithin: Server: template_id: {}, worker_id: {}",
-                        timeline_result_worker.template_id,
-                        timeline_result_worker.worker_id,
-                    )
-                },
-                LeafTimelineNode::TlLatestEventToState(timeline_result_worker) => {
-                    format!(
-                        "LeafTimeLine: TlEventToLatestState: Server: template_id: {}, worker_id: {}",
-                        timeline_result_worker.template_id,
-                        timeline_result_worker.worker_id,
-                    )
-                },
+        TypedTimelineResultWorker::LeafTimeline(leaf_time_line_node) => match leaf_time_line_node {
+            LeafTimelineNode::TlHasExisted(timeline_result_worker) => {
+                format!(
+                    "LeafTimeLine: TlHasExisted: Server: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
             }
-        }
-        TypedTimelineResultWorker::DerivedTimeline(derived_timeline) => {
-           match derived_timeline {
-               DerivedTimelineNode::EqualTo(timeline_result_worker) => {
-                   format!(
-                       "DerivedTimeLine: TlHasExisted: Server: template_id: {}, worker_id: {}",
-                       timeline_result_worker.template_id,
-                       timeline_result_worker.worker_id,
-                   )
-               },
-                DerivedTimelineNode::GreaterThan(timeline_result_worker) => {
-                     format!(
-                          "DerivedTimeLine: GreaterThan: Server: template_id: {}, worker_id: {}",
-                          timeline_result_worker.template_id,
-                          timeline_result_worker.worker_id,
-                     )
-                },
-                DerivedTimelineNode::GreaterThanOrEqualTo(timeline_result_worker) => {
-                     format!(
-                          "DerivedTimeLine: GreaterThanOrEqualTo: Server: template_id: {}, worker_id: {}",
-                          timeline_result_worker.template_id,
-                          timeline_result_worker.worker_id,
-                     )
-                },
-                DerivedTimelineNode::LessThan(timeline_result_worker) => {
-                     format!(
-                          "DerivedTimeLine: LessThan: Server: template_id: {}, worker_id: {}",
-                          timeline_result_worker.template_id,
-                          timeline_result_worker.worker_id,
-                     )
-                },
-               DerivedTimelineNode::LessThanOrEqualTo(timeline_result_worker) => {
-                     format!(
-                          "DerivedTimeLine: LessThanOrEqualTo: Server: template_id: {}, worker_id: {}",
-                          timeline_result_worker.template_id,
-                          timeline_result_worker.worker_id,
-                     )
-                },
-                DerivedTimelineNode::And(timeline_result_worker) => {
-                     format!(
-                          "DerivedTimeLine: And: Left: template_id: {}, worker_id: {}",
-                          timeline_result_worker.template_id,
-                          timeline_result_worker.worker_id,
-                     )
-                },
-                DerivedTimelineNode::Or(timeline_result_worker) => {
-                     format!(
-                          "DerivedTimeLine: Or: Left: template_id: {}, worker_id: {}",
-                          timeline_result_worker.template_id,
-                          timeline_result_worker.worker_id,
-                     )
-                },
-                DerivedTimelineNode::Not(timeline_result_worker) => {
-                     format!(
-                          "DerivedTimeLine: Not: Left: template_id: {}, worker_id: {}",
-                          timeline_result_worker.template_id,
-                          timeline_result_worker.worker_id,
-                     )
-                },
-
-           }
-        }
+            LeafTimelineNode::TlHasExistedWithin(timeline_result_worker) => {
+                format!(
+                    "LeafTimeLine: TlHasExistedWithin: Server: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+            LeafTimelineNode::TlLatestEventToState(timeline_result_worker) => {
+                format!(
+                    "LeafTimeLine: TlEventToLatestState: Server: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+        },
+        TypedTimelineResultWorker::DerivedTimeline(derived_timeline) => match derived_timeline {
+            DerivedTimelineNode::EqualTo(timeline_result_worker) => {
+                format!(
+                    "DerivedTimeLine: TlHasExisted: Server: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+            DerivedTimelineNode::GreaterThan(timeline_result_worker) => {
+                format!(
+                    "DerivedTimeLine: GreaterThan: Server: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+            DerivedTimelineNode::GreaterThanOrEqualTo(timeline_result_worker) => {
+                format!(
+                    "DerivedTimeLine: GreaterThanOrEqualTo: Server: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+            DerivedTimelineNode::LessThan(timeline_result_worker) => {
+                format!(
+                    "DerivedTimeLine: LessThan: Server: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+            DerivedTimelineNode::LessThanOrEqualTo(timeline_result_worker) => {
+                format!(
+                    "DerivedTimeLine: LessThanOrEqualTo: Server: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+            DerivedTimelineNode::And(timeline_result_worker) => {
+                format!(
+                    "DerivedTimeLine: And: Left: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+            DerivedTimelineNode::Or(timeline_result_worker) => {
+                format!(
+                    "DerivedTimeLine: Or: Left: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+            DerivedTimelineNode::Not(timeline_result_worker) => {
+                format!(
+                    "DerivedTimeLine: Not: Left: component_id: {}, worker_id: {}",
+                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
+                )
+            }
+        },
     }
 }
--- timeline/src/timeline_node_worker.rs
+++ timeline/src/timeline_node_worker.rs
@@ -1,11 +1,10 @@
-use std::fmt::Display;
 use serde::Serialize;
-
+use std::fmt::Display;
 
 #[derive(Clone, Debug)]
 pub struct TimeLineNodeWorkerInput {
     pub worker_id_prefix: TimeLineWorkerIdPrefix,
-    pub template_id: String,
+    pub component_id: String,
 }
 
 #[derive(Clone, Debug)]
@@ -19,7 +18,7 @@ impl Display for TimeLineWorkerIdPrefix {
 
 impl Display for TimeLineNodeWorkerInput {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}.{}", self.template_id, self.worker_id_prefix)
+        write!(f, "{}.{}", self.component_id, self.worker_id_prefix)
     }
 }
 
@@ -39,7 +38,7 @@ impl Display for TimeLineWorkerId {
 #[derive(Clone, Debug, Serialize)]
 pub struct TimeLineResultWorker {
     pub worker_id: TimeLineWorkerId,
-    pub template_id: String,
+    pub component_id: String,
 }
 
 // This not only says the worker in which result is available,
@@ -47,55 +46,55 @@ pub struct TimeLineResultWorker {
 #[derive(Clone, Debug, Serialize)]
 pub enum TypedTimeLineResultWorker {
     LeafTimeLine(LeafTimeLineNode),
-    DerivedTimeLine(DerivedTimeLineNode)
+    DerivedTimeLine(DerivedTimeLineNode),
 }
 
 impl TypedTimeLineResultWorker {
     pub fn tl_has_existed(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::LeafTimeLine(LeafTimeLineNode::TLHasExisted {
-            time_line_worker: worker
+            time_line_worker: worker,
         })
     }
 
     pub fn tl_has_existed_within(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::LeafTimeLine(LeafTimeLineNode::TLHasExistedWithin {
-            time_line_worker: worker
+            time_line_worker: worker,
         })
     }
 
     pub fn tl_event_to_latest_state(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::LeafTimeLine(LeafTimeLineNode::TLEventToLatestState {
-            time_line_worker: worker
+            time_line_worker: worker,
         })
     }
 
     pub fn equal_to(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::DerivedTimeLine(DerivedTimeLineNode::EqualTo {
-            result_worker: worker
+            result_worker: worker,
         })
     }
 
     pub fn greater_than(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::DerivedTimeLine(DerivedTimeLineNode::GreaterThan {
-            result_worker: worker
+            result_worker: worker,
         })
     }
 
     pub fn greater_than_or_equal_to(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::DerivedTimeLine(DerivedTimeLineNode::GreaterThanOrEqualTo {
-            result_worker: worker
+            result_worker: worker,
         })
     }
 
     pub fn less_than(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::DerivedTimeLine(DerivedTimeLineNode::LessThan {
-            result_worker: worker
+            result_worker: worker,
         })
     }
 
     pub fn less_than_or_equal_to(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::DerivedTimeLine(DerivedTimeLineNode::LessThanOrEqualTo {
-            result_worker: worker
+            result_worker: worker,
         })
     }
 
@@ -113,7 +112,7 @@ impl TypedTimeLineResultWorker {
 
     pub fn not(worker: TimeLineResultWorker) -> TypedTimeLineResultWorker {
         TypedTimeLineResultWorker::DerivedTimeLine(DerivedTimeLineNode::Not {
-            result_worker: worker
+            result_worker: worker,
         })
     }
 }
@@ -135,31 +134,15 @@ pub enum LeafTimeLineNode {
 
 #[derive(Clone, Debug, Serialize)]
 pub enum DerivedTimeLineNode {
-    EqualTo {
-        result_worker: TimeLineResultWorker,
-    },
-    GreaterThan {
-        result_worker: TimeLineResultWorker,
-    },
-    GreaterThanOrEqualTo {
-        result_worker: TimeLineResultWorker,
-    },
-    LessThan {
-        result_worker: TimeLineResultWorker,
-    },
-    LessThanOrEqualTo {
-        result_worker: TimeLineResultWorker,
-    },
+    EqualTo { result_worker: TimeLineResultWorker },
+    GreaterThan { result_worker: TimeLineResultWorker },
+    GreaterThanOrEqualTo { result_worker: TimeLineResultWorker },
+    LessThan { result_worker: TimeLineResultWorker },
+    LessThanOrEqualTo { result_worker: TimeLineResultWorker },
 
-    And {
-        result_worker: TimeLineResultWorker,
-    },
+    And { result_worker: TimeLineResultWorker },
 
-    Or {
-        result_worker: TimeLineResultWorker,
-    },
+    Or { result_worker: TimeLineResultWorker },
 
-    Not {
-        result_worker: TimeLineResultWorker
-    },
+    Not { result_worker: TimeLineResultWorker },
 }
