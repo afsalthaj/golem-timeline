a03544c252e9d97d2bec9ee2e4d96d7877a21b9b
kmatas <33095685+kmatasfp@users.noreply.github.com>
2024-05-08 15:06:33 -0700

    fix the url we use to call `invoke-and-await`

--- sample-event-feeder/src/main.rs
+++ sample-event-feeder/src/main.rs
@@ -1,17 +1,14 @@
 use std::env;
-use std::fmt::Error;
 
 use futures::TryStreamExt;
 use log;
 use pulsar::{
-    Authentication, authentication::oauth2::OAuth2Authentication, Consumer, DeserializeMessage,
+    authentication::oauth2::OAuth2Authentication, Authentication, Consumer, DeserializeMessage,
     Payload, Pulsar, SubType, TokioExecutor,
 };
-use pulsar::error::AuthenticationError::Custom;
 use reqwest::Client;
-use reqwest::header::{CONTENT_TYPE, HeaderName, HeaderValue};
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde_json::json;
 use tokio;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -22,7 +19,7 @@ pub struct Event {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "kebab-case")]
-pub enum EventValue{
+pub enum EventValue {
     StringValue(String),
     IntValue(i64),
     FloatValue(f64),
@@ -39,7 +36,7 @@ impl DeserializeMessage for Event {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct CustomBody {
-    params: Vec<Event>
+    params: Vec<Event>,
 }
 
 #[tokio::main]
@@ -82,56 +79,33 @@ async fn main() -> Result<(), pulsar::Error> {
 
     let client = Client::new();
 
-
     let component_id = env::var("COMPONENT_ID").expect("Provide COMPONENT_ID. You can understand from the output logs of quick_test.sh which was used to register timeline with Golem");
     let worker_name = env::var("WORKER_NAME").expect("Provide WORKER_NAME. This should correspond to the worker that directly process events. You can understand from the output logs of quick_test.sh which was used to register timeline with Golem");
-
-
-    let url = format!("http://localhost:9005/v2/components/{}/workers/{}/key", component_id, worker_name);
 
     let mut counter = 0usize;
     while let Some(msg) = consumer.try_next().await? {
         consumer.ack(&msg).await?;
         log::info!("metadata: {:?}", msg.metadata());
         log::info!("id: {:?}", msg.message_id());
-        let data = match msg.deserialize() {
+        match msg.deserialize() {
             Ok(data) => {
-                dbg!("got {} messages", data.clone());
-
-
-                let response = client
-                    .post(&url)
-                    .send()
-                    .await.map_err(|error| pulsar::Error::Custom(error.to_string()))?;
-
-
-                let response_text = response.text().await.map_err(|error| pulsar::Error::Custom(error.to_string()))?;
-                let value: Value = serde_json::from_str(&response_text).map_err(|error| pulsar::Error::Custom(error.to_string()))?;
-
-                // Extract value from response
-                let invocation_key =
-                    value["value"].as_str().ok_or(pulsar::Error::Custom("Missing value field".to_string()))?;
-
-                dbg!("The invocation_key key is: {}", invocation_key);
-
                 let invoke_url =
-                    format!("http://localhost:9005/v2/components/{}/workers/{}/invoke-and-await?invocation-key={}&function={}", component_id, worker_name, invocation_key, "timeline:event-processor/api/add-event");
-
+                    format!("http://localhost:9005/v2/components/{}/workers/{}/invoke-and-await?function={}", component_id, worker_name, "timeline:event-processor/api/add-event");
 
                 let params = json!({"params": [data]});
 
                 dbg!(params.to_string());
 
                 // Second POST request
-                 let result = client
-                     .post(&invoke_url)
-                     .json(&params)
-                     .send()
-                     .await.map_err(|error| pulsar::Error::Custom(error.to_string()))?;
+                let result = client
+                    .post(&invoke_url)
+                    .json(&params)
+                    .send()
+                    .await
+                    .map_err(|error| pulsar::Error::Custom(error.to_string()))?;
 
                 dbg!(result);
-
-            },
+            }
             Err(e) => {
                 log::error!("could not deserialize message: {:?}", e);
                 break;
@@ -139,7 +113,6 @@ async fn main() -> Result<(), pulsar::Error> {
         };
 
         counter += 1;
-
 
         if counter > 10 {
             consumer.close().await.expect("Unable to close consumer");
