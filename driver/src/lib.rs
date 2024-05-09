f20f6dfcf10c3a3a6eab36218c9dee14dc8dd41e
Afsal Thaj <afsal.taj06@gmail.com>
2024-05-09 01:26:26 -0600

    Avoid conversions

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -49,36 +49,6 @@ impl Guest for Component {
         match core.initialize_timeline(&timeline_op) {
             Ok(result) => {
                 dbg!("Driver Log: Timeline initialized");
-
-                // let serializable_result_worker = serde_json::to_value(
-                //     timeline::timeline_node_worker::TypedTimeLineResultWorker::from_wit(
-                //         result.result_worker,
-                //     ),
-                // )
-                // .map_err(|err| err.to_string())?;
-                //
-                // let mut event_processors = vec![];
-                //
-                // for worker in result.event_processor_workers.iter() {
-                //     let event_processor =
-                //         timeline::timeline_node_worker::TypedTimeLineResultWorker::from_wit(
-                //             worker.clone(),
-                //         );
-                //     event_processors.push(
-                //         serde_json::to_value(event_processor).map_err(|err| err.to_string())?,
-                //     );
-                // }
-                //
-                // let driver_result: String =
-                //     serde_json::Value::Object(serde_json::Map::from_iter(vec![
-                //         ("result_worker".to_string(), serializable_result_worker),
-                //         (
-                //             "event_processors".to_string(),
-                //             serde_json::Value::Array(event_processors),
-                //         ),
-                //     ]))
-                //     .to_string();
-
                 Ok(result)
             }
             Err(error) => {
@@ -86,82 +56,5 @@ impl Guest for Component {
                 Err(error)
             }
         }
-    }
-}
-
-fn print_typed_time_line_result_worker(
-    typed_timeline_result_worker: TypedTimelineResultWorker,
-) -> String {
-    match typed_timeline_result_worker {
-        TypedTimelineResultWorker::LeafTimeline(leaf_time_line_node) => match leaf_time_line_node {
-            LeafTimelineNode::TlHasExisted(timeline_result_worker) => {
-                format!(
-                    "LeafTimeLine: TlHasExisted: Server: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            LeafTimelineNode::TlHasExistedWithin(timeline_result_worker) => {
-                format!(
-                    "LeafTimeLine: TlHasExistedWithin: Server: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            LeafTimelineNode::TlLatestEventToState(timeline_result_worker) => {
-                format!(
-                    "LeafTimeLine: TlEventToLatestState: Server: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-        },
-        TypedTimelineResultWorker::DerivedTimeline(derived_timeline) => match derived_timeline {
-            DerivedTimelineNode::EqualTo(timeline_result_worker) => {
-                format!(
-                    "DerivedTimeLine: TlHasExisted: Server: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            DerivedTimelineNode::GreaterThan(timeline_result_worker) => {
-                format!(
-                    "DerivedTimeLine: GreaterThan: Server: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            DerivedTimelineNode::GreaterThanOrEqualTo(timeline_result_worker) => {
-                format!(
-                    "DerivedTimeLine: GreaterThanOrEqualTo: Server: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            DerivedTimelineNode::LessThan(timeline_result_worker) => {
-                format!(
-                    "DerivedTimeLine: LessThan: Server: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            DerivedTimelineNode::LessThanOrEqualTo(timeline_result_worker) => {
-                format!(
-                    "DerivedTimeLine: LessThanOrEqualTo: Server: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            DerivedTimelineNode::And(timeline_result_worker) => {
-                format!(
-                    "DerivedTimeLine: And: Left: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            DerivedTimelineNode::Or(timeline_result_worker) => {
-                format!(
-                    "DerivedTimeLine: Or: Left: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-            DerivedTimelineNode::Not(timeline_result_worker) => {
-                format!(
-                    "DerivedTimeLine: Not: Left: component_id: {}, worker_id: {}",
-                    timeline_result_worker.template_id, timeline_result_worker.worker_id,
-                )
-            }
-        },
     }
 }
