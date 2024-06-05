a81155d8210ab9cf769a8b5f97981caa285e7d50
Afsl Thaj <afsal.taj06@gmail.com>
2024-06-05 21:54:52 +1000

    Remove unwanted file

--- sample.sh
+++
@@ -1,6 +0,0 @@
-if git show origin/github_pages:benchmark_cold_storage_large.json > pulled_file.txt; then
-  cat pulled_file.txt
-else
-  echo "Failed to pull the file."
-  # Add your alternative actions here
-fi
