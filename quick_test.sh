80f5930cd6448f14f1b57b939d9656c64792e2ae
Afsal Thaj <afsal.taj06@gmail.com>
2025-03-19 22:21:12 +1100

    Make sure timeline project works

--- quick_test.sh
+++ quick_test.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 golem component deploy
-
-# Construct the command with properly formatted parameters
-command="golem worker invoke timeline:driver/first-try timeline:driver/api.{run}"
-
-echo "Invoking Driver with a TimeLineOp to initialise the whole workflow"
-echo "$command"
-eval $command
-echo "A sample invocation succeeded!"
+golem worker invoke timeline:driver/first-try timeline:driver/api.{run}
